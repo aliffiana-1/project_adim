2126fdb24a2c395aa68be59ab61696ac5d3304ef
DESKTOP-8I581UN\Lenovo <vitaapriliana24@gmail.com>
2026-01-24 21:08:40 +0700

    add : penambahan migration

---
+++ database/migrations/2026_01_24_134054_create_articles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('article_level_id')->constrained('article_levels');
+
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('is_published')->default(false);
+
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
