24cc1e5652e3831915c8e35272045687714ed37a
DESKTOP-8I581UN\Lenovo <vitaapriliana24@gmail.com>
2026-01-25 13:05:03 +0700

    add : penambahan model role

---
+++ app/Models/Roles.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $table = 'roles';
+    protected $primaryKey = 'id_role';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'role_name',
+    ];
+
+    /**
+     * Relasi ke tabel users
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class, 'id_role', 'id_role');
+    }
+}
